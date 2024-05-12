5af92a73e0ce261f6669c24adcf1a24ab211dd08
Sizant <superkop76@gmail.com>
2024-05-12 20:55:24 +0530

    backend logic

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,12 @@
+"use client"
 import TaskList from "@/components/TaskList";
 import SignIn from "@/components/SignIn";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [newUser, setNewUser] = useState(false);
+ 
   
   return (
     <>
@@ -14,12 +19,28 @@ export default function Home() {
             alt="Your Company"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
+          {   !newUser?"Sign in to your account":'Create a new account'}
           </h2>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" action="#" method="POST">
+           {newUser&& <div>
+              <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                Username
+              </label>
+              <div className="mt-2">
+                <input
+                  id="username"
+                  name="username"
+                  type="username"
+                  autoComplete="username"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>}
+
             <div>
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                 Email address
@@ -41,11 +62,7 @@ export default function Home() {
                 <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
                   Password
                 </label>
-                <div className="text-sm">
-                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                    Forgot password?
-                  </a>
-                </div>
+                
               </div>
               <div className="mt-2">
                 <input
@@ -64,13 +81,13 @@ export default function Home() {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Sign in
+                {!newUser?"Sign in":"Sign up"}
               </button>
             </div>
           </form>
 
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{' '}
+          <p onClick={()=>setNewUser((prev)=>!prev)}  className="mt-10 text-center text-sm text-gray-500 cursor-pointer">
+            {   !newUser?"Not a member?":'Already have an account?'}
           </p>
         </div>
       </div>
--- app/tasklist/page.jsx
+++ app/tasklist/page.jsx
@@ -25,7 +25,7 @@ export default async function TaskList() {
   const {tasks} = await getTasks();
   return( 
   <>
-  {/* < Navbar /> */}
+ 
   {tasks.map((t)=>(
   <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
     <div>
